72628094ba236492f4f443cd2c19c9ef620f0320
Fabian <b9rfrr@gmail.com>
2024-10-07 13:48:03 -0600

    Listo mi ejercicio

---
+++ script.js
@@ -0,0 +1,12 @@
+let nombre = prompt("¿Cual es tu nombre?");
+let edad = prompt("¿Cuantos años tienes?");
+let estudiaste = false;
+
+console.log(nombre)
+console.log(edad)
+console.log(estudiaste)
+
+let mensaje = `Hola soy ${nombre} y tengo ${edad} años`
+console.log(mensaje)
+
+if (estudiaste >=true) {alert ("si estudiaste pasaras la materia")} else {alert ("No estudiaste reprobaras la materia")}
